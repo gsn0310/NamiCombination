07e58a7827810756bbc969f06406023ed7b359d9
lanou\lanou <1666972845@qq.com>
2019-10-11 10:28:00 +0800

    no message

--- vueproject/src/com/Home.vue
+++ vueproject/src/com/Home.vue
@@ -1,8 +1,12 @@
 <template>
-    $END$
+  <div>
+
+  </div>
 </template>
 
 <script>
+  import { Header } from 'mint-ui';
+  Vue.component(Header.name, Header);
     export default {
         name: "Home"
     }
--- vueproject/src/router/index.js
+++ vueproject/src/router/index.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Homg from
 
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
-
+    {path:"/",component:Home}
   ]
 })
